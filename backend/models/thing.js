19f7b2522cafb47c7f9ae3d0c8ed3e681b00a2ef
jackwonka <lstephane333@gmail.com>
2020-12-08 11:36:28 +0100

    Modif model

--- backend/models/thing.js
+++ backend/models/thing.js
@@ -1,11 +1,21 @@
+// Mongoose créer un modèle de données
 const mongoose = require('mongoose');
 
-const thingSchema = mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  userId: { type: String, required: true },
-  price: { type: Number, required: true },
+// Modèle de données
+const sauceSchema = mongoose.Schema({
+    
+    name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    description: { type: String, required: true },
+    mainPepper: { type: String, required: true },
+    imageUrl: { type: String },
+    heat: { type: Number },
+    likes: { type: Number, default: 0},
+    dislikes: { type: Number, default: 0 },
+    userId: { type: String },
+    usersLiked: [String],
+    usersDisliked: [String] ,
+    
 });
 
-module.exports = mongoose.model('Thing', thingSchema);
+module.exports = mongoose.model('Sauce', sauceSchema);
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
+
+// Valide l'email unique
 const uniqueValidator = require('mongoose-unique-validator');
 
+// Deux utilisateurs ne peuvent pas utiliser la même adresse mail
 const userSchema = mongoose.Schema({
+  // Adresse mail + user unique
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
 });
 
-userSchema.plugin(uniqueValidator);
+userSchema.plugin(uniqueValidator);// Rajouter plugin uniquevalidateur à userschema
 
 module.exports = mongoose.model('User', userSchema);
